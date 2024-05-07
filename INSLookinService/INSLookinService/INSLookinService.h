3544964810bb842d1d57b5d3fa69065c95bdb43f
李智洪 <lizhihong@lizhihongdeMac-mini.local>
2024-05-07 18:16:13 +0800

    添加carthage的支持

---
+++ INSLookinService/INSLookinService/INSLookinService.h
@@ -0,0 +1,18 @@
+//
+//  INSLookinService.h
+//  INSLookinService
+//
+//  Created by 李智洪 on 2024/5/7.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for INSLookinService.
+FOUNDATION_EXPORT double INSLookinServiceVersionNumber;
+
+//! Project version string for INSLookinService.
+FOUNDATION_EXPORT const unsigned char INSLookinServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <INSLookinService/PublicHeader.h>
+
+
